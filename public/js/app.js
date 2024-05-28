e5b9d6ae8148e4ec3f8d836df70ad7e7ea13deb0
Abdumajid-777 <abdumajid951@gmail.com>
2024-05-28 10:32:16 +0500

    123

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+// let arrowBtn = document.querySelector("#arrowBtn")
+// let shrink = document.querySelector("#shrink")
+
+
+// arrowBtn.addEventListener('click' , function(){
+//     shrink.classList.toggle("")
+// })
