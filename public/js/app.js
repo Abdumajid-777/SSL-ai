1830fb3e3a9df5afd8d4c0c1a9e68cb1a6883181
Abdumajid-777 <abdumajid951@gmail.com>
2024-06-03 16:00:32 +0500

    1234

--- public/js/app.js
+++ public/js/app.js
@@ -13,3 +13,18 @@ overflowBtn.forEach(function (item, index) {
     // overflowBox[index].classList.toggle("hidden")
     // overflowBox[index].classList.remove("flex")
 })
+
+
+let closeMenu = document.querySelector("#closeMenu")
+let hamburgerMenu = document.querySelector("#hamburgerMenu")
+let responsiveBox = document.querySelector("#responsiveBox")
+
+
+hamburgerMenu.addEventListener('click' , function(){
+    responsiveBox.classList.add("right-0")
+    responsiveBox.classList.remove("-right-full")
+})
+closeMenu.addEventListener('click' , function(){
+    responsiveBox.classList.add("-right-full")
+    responsiveBox.classList.remove("right-0")
+})
